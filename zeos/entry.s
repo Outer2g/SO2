dbf5d670c098f01c42e221badfb99d1ede433446
Outer2g <aol.treballs@gmail.com>
2016-09-23 11:42:27 +0200

    mov solved

--- zeos/entry.s
+++ zeos/entry.s
@@ -12,10 +12,10 @@
 # 6 "entry.S" 2
 # 1 "include/segment.h" 1
 # 7 "entry.S" 2
-# 72 "entry.S"
-keyboard_handler:
+# 85 "entry.S"
+.globl keyboard_handler; .type keyboard_handler, @function; .align 0; keyboard_handler:
         pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es
         movb $0x20,%al ; outb %al,$0x20 ;
         call keyboard_routine
-        movl %gs,28(%esp); movl %fs,24(%esp); movl %es,20(%esp); movl %ds,1C(%esp); movl %eax,18(%esp); movl %ebp,14(%esp); movl %edi,10(%esp); movl %esi,C(%esp); movl %edx,8(%esp); movl %ecx,4(%esp); movl %ebx,(%esp)
-        ret
+        popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs
+        iret

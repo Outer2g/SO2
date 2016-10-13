058a399b02ecf314dc68258af5d9d350852ec4ef
Outer2g <aol.treballs@gmail.com>
2016-10-13 13:56:44 +0200

    init_idle done, init_task1 done

--- zeos/sched.c
+++ zeos/sched.c
@@ -14,6 +14,7 @@ union task_union protected_tasks[NR_TASKS+2]
   __attribute__((__section__(".data.task")));
 
 union task_union *task = &protected_tasks[1]; /* == union task_union task[NR_TASKS] */
+struct task_struct *idle_task;
 
 #if 0
 struct task_struct *list_head_to_task_struct(struct list_head *l)
@@ -59,11 +60,43 @@ void cpu_idle(void)
 
 void init_idle (void)
 {
-
+    //PID process = 0, kernel mode only, only if there's not another process to execute
+    //PID = 0
+    struct list_head * e = list_first( &freequeue );
+    union task_union * realelement = list_entry( e, struct task_struct, list);
+    realelement->task.PID = 0;
+    allocate_DIR(&realelement->task);
+    idle_task = &realelement->task;
+    
+    // init stack
+    //push &cpu_idle
+    int pos = KERNEL_STACK_SIZE - 1;
+    realelement->stack[pos] = &cpu_idle;
+    //push initial value to ebp (0)
+    --pos;
+    realelement->stack[pos] = 0x00000000;
+    realelement->task.kernel_esp = pos;
+    
+    //delete the task from the freequeue
+    list_del(e);
+    
 }
 
 void init_task1(void)
 {
+    //PID process = 1,user code, code is in user.c
+    //PID = 1
+    struct list_head * e = list_first( &freequeue );
+    union task_union * realelement = list_entry( e, struct task_struct, list);
+    realelement->task.PID = 1;
+    allocate_DIR(&realelement->task);
+    //init user address space
+    set_user_pages(&realelement->task);
+    tss.esp0 = &realelement->stack[KERNEL_STACK_SIZE];
+    //set page directory as the current
+    set_cr3(realelement->task.dir_pages_baseAddr);
+    //delete task from the freequeue
+    list_del(e);
 }
 
 
@@ -84,6 +117,28 @@ void init_freequeue(){
         ++aux;
         ++current;
     }
+}
+void inner_task_switch(union task_union* new){
+    //update tss
+    tss.esp0 = &new->stack[KERNEL_STACK_SIZE];
+    //update user mem
+    set_cr3(new->task.dir_pages_baseAddr);
+    asn("push %ebp;"
+        "movl %esp,%ebp;"
+    );
+    
+}
+void task_switch(union task_union* new){
+    //new pointer to the task_union of the process that will be executed 
+    //saving registers esi edi ebx
+    	
+    asm("pushl %esi;"
+        "pushl %edi;"
+        "pushl %ebx");
+    inner_task_switch(new);
+    asm("popl %esi;"
+        "popl %edi;"
+        "popl %ebx;");
 }
 struct task_struct* current()
 {
